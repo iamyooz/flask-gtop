ee4799fbc365849b0f6e7c71dbd35fd179d2b7f9
unknown <yujin23@sookmyung.ac.kr>
2023-08-31 20:34:00 +0900

    first commit

---
+++ gtop.py
@@ -0,0 +1,56 @@
+import json
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import joblib
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+app = Flask(__name__)
+CORS(app)
+
+kmeans_model = joblib.load('gtop_kmeans_model.pkl')
+scaler = None
+learning_data = None
+
+def load_scaler_and_data():
+    global scaler, learning_data
+    if scaler is None:
+        with open('girlish_top_cluster.json', 'r') as json_file:
+            learning_data = json.load(json_file)
+
+        temperature_data = [[item['average_temperature'], item['temperature_difference']] for item in learning_data]
+
+        scaler = StandardScaler()
+        scaler.fit(temperature_data)
+
+@app.route('/', methods=['POST'])
+def get_cluster_images():
+    global scaler, learning_data
+
+    try:
+        if request.method == 'POST' and request.is_json:
+            data = request.json
+            load_scaler_and_data()
+
+            input_data = [[data['average_temperature'], data['temperature_difference']]]
+            input_data_scaled = scaler.transform(input_data)
+
+            cluster_label = kmeans_model.predict(input_data_scaled)[0]
+
+            cluster_images = []
+            for item in learning_data:
+                if item['cluster'] == cluster_label:
+                    cluster_images.append(item['image'])
+
+            response = {
+                'cluster_images': cluster_images
+            }
+
+            return jsonify(response), 200
+        else:
+            return jsonify({'error': 'Invalid request'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
